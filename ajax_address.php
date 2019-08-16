b692321aab4b6123de05c2e425df30046170305d
cloudsky21 <requiemofsouls18@gmail.com>
2019-08-16 11:36:33 +0800

    address query adjusted

--- ajax_address.php
+++ ajax_address.php
@@ -5,16 +5,13 @@ include("connection/conn.php");
 
 $peo = htmlentities($_POST['id']);
 
-	$result = $db->prepare("SELECT town FROM location WHERE province = ? GROUP BY town");
-	$result->execute([$peo]);
+$result = $db->prepare("SELECT town FROM location WHERE province = ? ORDER BY town");
+$result->execute([$peo]);
 
 
 
-		foreach($result as $row){
-	
-	
-			echo '<option value="'.$row['town'].'">'.$row['town'].'</option>';
-		}
+foreach ($result as $row) {
 
 
-?>
+	echo '<option value="' . $row['town'] . '">' . $row['town'] . '</option>';
+}
